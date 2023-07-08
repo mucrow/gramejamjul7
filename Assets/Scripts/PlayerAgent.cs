d79814bfcb18f92f19734da884d18c1a5c8e721d
mucrow <120820315+mucrow@users.noreply.github.com>
2023-07-08 18:48:17 -0400

    Left click shoots arrow from camera ! Right click slows down time !

---
+++ Assets/Scripts/PlayerAgent.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class PlayerAgent: MonoBehaviour {
+  [SerializeField] GameObject _arrowPrefab;
+
+  Camera _mainCamera;
+
+  void Start() {
+    _mainCamera = Camera.main;
+  }
+
+  void Update() {
+    bool shootArrowButtonPressed = Input.GetMouseButtonDown(0);
+    bool slowTimeButtonPressed = Input.GetMouseButtonDown(1);
+    bool slowTimeButtonReleased = Input.GetMouseButtonUp(1);
+
+    if (shootArrowButtonPressed) {
+      var newArrow = Utils.FireProjectile(_mainCamera.transform, _arrowPrefab, Vector3.zero);
+
+      var arrowRotation = newArrow.transform.eulerAngles;
+      arrowRotation.x = 0f;
+      arrowRotation.z = 0f;
+      newArrow.transform.eulerAngles = arrowRotation;
+
+      var arrowPosition = newArrow.transform.position;
+      // TODO choose Y-level based on MAXIMUM KILL POTENTIAL
+      arrowPosition.y = 0.5f;
+      newArrow.transform.position = arrowPosition;
+    }
+
+    if (slowTimeButtonPressed) {
+      Time.timeScale = 0.1f;
+    }
+    if (slowTimeButtonReleased) {
+      Time.timeScale = 1f;
+    }
+  }
+}
