a399d594e1acffd3a314e1320cdd2b768910246f
mucrow <120820315+mucrow@users.noreply.github.com>
2023-07-09 15:53:56 -0400

    My aim is True...

--- Assets/Scripts/PlayerAgent.cs
+++ Assets/Scripts/PlayerAgent.cs
@@ -1,32 +1,34 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class PlayerAgent: MonoBehaviour {
   [SerializeField] GameObject _arrowPrefab;
   [SerializeField] float _slowMotionTimeScale = 0.2f;
 
+  LineRenderer _lineRenderer;
   Camera _mainCamera;
 
   void Start() {
     _mainCamera = Camera.main;
+    _lineRenderer = GetComponentInChildren<LineRenderer>();
   }
 
   void Update() {
-    bool shootArrowButtonPressed = Input.GetMouseButtonDown(0);
+    var arrowRayStart = _mainCamera.transform.position;
+    arrowRayStart.y = 1f;
+    var arrowRayEulerAngles = new Vector3(0f, _mainCamera.transform.eulerAngles.y, 0f);
+    var arrowRayDirection = Quaternion.Euler(arrowRayEulerAngles);
+    var arrowRayPositionDelta = arrowRayDirection * Vector3.forward * 1000f;
+
+    var arrowRayEnd = arrowRayStart + arrowRayPositionDelta;
+    _lineRenderer.SetPositions(new Vector3[] { arrowRayStart, arrowRayEnd });
+
+    bool strafeArrowButtonPressed = Input.GetMouseButtonDown(0);
     bool slowTimeButtonPressed = Input.GetMouseButtonDown(1);
     bool slowTimeButtonReleased = Input.GetMouseButtonUp(1);
 
-    if (shootArrowButtonPressed) {
-      var newArrow = Utils.FireProjectile(_mainCamera.transform, _arrowPrefab, Vector3.zero);
-
-      var arrowRotation = newArrow.transform.eulerAngles;
-      arrowRotation.x = 0f;
-      arrowRotation.z = 0f;
-      newArrow.transform.eulerAngles = arrowRotation;
-
-      var arrowPosition = newArrow.transform.position;
-      // TODO choose Y-level based on MAXIMUM KILL POTENTIAL
-      arrowPosition.y = 1f;
-      newArrow.transform.position = arrowPosition;
+    if (strafeArrowButtonPressed) {
+      FireArrow(arrowRayStart, arrowRayDirection);
     }
 
     if (slowTimeButtonPressed) {
@@ -34,6 +36,22 @@ public class PlayerAgent: MonoBehaviour {
     }
     if (slowTimeButtonReleased) {
       Time.timeScale = 1f;
+    }
+  }
+
+  void FireArrow(Vector3 arrowStart, Quaternion arrowDirection) {
+    var raycastHits = Physics.RaycastAll(arrowStart, arrowDirection * Vector3.forward);
+    // sort raycast hits by distance
+    Array.Sort(raycastHits, new RaycastHitAscendingDistanceComparer());
+
+    foreach (var raycastHit in raycastHits) {
+      var hitCollider = raycastHit.collider;
+      if (!hitCollider.isTrigger) {
+        break;
+      }
+      if (hitCollider.CompareTag("Enemy")) {
+        Destroy(hitCollider.gameObject);
+      }
     }
   }
 }
---
+++ Assets/Scripts/RaycastHitAscendingDistanceComparer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastHitAscendingDistanceComparer: IComparer<RaycastHit> {
+  public int Compare(RaycastHit a, RaycastHit b) {
+    // ReSharper disable once CompareOfFloatsByEqualityOperator
+    if (a.distance == b.distance) {
+      return 0;
+    }
+    if (a.distance > b.distance) {
+      return 1;
+    }
+    return -1;
+  }
+}
