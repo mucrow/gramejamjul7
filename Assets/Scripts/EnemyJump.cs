f5a799778ef78c36504de6023ad7ce4419f14bc6
ahodori <supersamfast@gmail.com>
2023-07-09 16:03:58 -0400

    jumping

---
+++ Assets/Scripts/EnemyJump.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyJump : MonoBehaviour {
+    [SerializeField] float jumpInterval;
+    [SerializeField] float jumpStrength;
+    [SerializeField] float gravityStrength;
+    Rigidbody _rigidbody;
+
+    float _timeSinceLastJump;
+    float _startingY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _timeSinceLastJump = 0;
+        _rigidbody = GetComponent<Rigidbody>();
+        _startingY = transform.position.y;
+    }
+
+    void FixedUpdate()
+    {
+        _timeSinceLastJump += Time.fixedDeltaTime;
+        if (_timeSinceLastJump > jumpInterval) {
+            _timeSinceLastJump = 0;
+            Debug.Log("YUMP");
+            _rigidbody.AddForce(_rigidbody.transform.up * jumpStrength);
+            //transform.position += Vector3.up * 1;
+        }
+
+        if (transform.position.y < _startingY) {
+            _rigidbody.velocity += Vector3.up * -1 * _rigidbody.velocity.y;
+            transform.position += Vector3.up * (_startingY - transform.position.y);
+        } else {
+            _rigidbody.velocity += Vector3.down * gravityStrength;
+        }
+    }
+}
