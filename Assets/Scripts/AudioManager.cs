021326ad4a24db988f257c5243be4e8ffd1f8eae
mucrow <120820315+mucrow@users.noreply.github.com>
2023-07-09 23:14:51 -0400

    audiomanager for less music resets

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager: MonoBehaviour {
+  public static AudioManager Instance;
+
+  [SerializeField] AudioSource _musicSource;
+
+  void Awake() {
+    if (Instance == null) {
+      Instance = this;
+      DontDestroyOnLoad(gameObject);
+      _musicSource = GetComponent<AudioSource>();
+    }
+    else {
+      Destroy(gameObject);
+    }
+  }
+
+  public void PlaySong(AudioClip song) {
+    if (_musicSource.clip == song && _musicSource.isPlaying) {
+      return;
+    }
+    _musicSource.clip = song;
+    _musicSource.Play();
+  }
+}
---
+++ Assets/Scripts/LevelMusic.cs
@@ -0,0 +1,10 @@
+﻿
+using UnityEngine;
+
+public class LevelMusic: MonoBehaviour {
+  [SerializeField] AudioClip _song;
+
+  void Start() {
+    AudioManager.Instance.PlaySong(_song);
+  }
+}
